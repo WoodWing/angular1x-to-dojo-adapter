87a77bfafb6c58dde6ae5072d7f1d508f2af5bb9
Erwin Verdonk <e.verdonk@gmail.com>
2015-12-01 11:32:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# angular1x-to-dojo-adapter
+Adapter to let Angular 1.x directives work within a Dojo application.
+## Install / Setup
+To make use of this adapter you will have to install it through NPM first. You can do that with: ```bower install angular1x-to-dojo-adapter```
+You will have to load some JavaScript files in the Dojo application in which you want to use this adapter. The following files should be loaded (paths can be different for your application configuration):
+```html
+<script type='text/javascript' src="path/to/bower_components/angular/angular.min.js.js"></script>
+<script type='text/javascript' src="path/to/bower_components/angular1x-to-dojo-adapter/dist/angular1x-to-dojo-adapter.min.js"></script>
+```
+## Usage
+All 'data-dojo-props' properties of Dojo will be added as attribute binding to the DOM element for Angular to compile. The module of the directive and the module dependencies should be added as a Dojo property names '$modules' in 'data-dojo-props'. The value for this property can be either a single String or an Array of Strings.
+##### Use an Angular directive component with data passed from Dojo.
+In this example you see that we pass people data to the property/attribute 'people', which is used within the Angular directive component to populate the list with people.
+```html
+<people-directive data-dojo-type="AngularToDojoAdapter"
+	data-dojo-props="people: this.peopleData, $modules: 'people'">
+</people-directive>
+```
+##### Use an Angular directive component with events.
+In this example we show how to use events which are triggered by Angular but handled in Dojo.
+```html
+<people-directive data-dojo-type="AngularToDojoAdapter"
+	data-dojo-props="data: this.peopleData, onClick: this.onClick, $modules: 'people'"
+	onclick="this.onClick(event)">
+</people-directive>
+```
+##### Use an Angular directive component with other directives.
+Here we added the 'ng-class' directive to add a CSS class when mobile is used. The boolean for the mobile check is retrieved from an Angular controller.
+```html
+<people-directive data-dojo-type="AngularToDojoAdapter"
+    ng-class="{mobile: vm.isMobile}"
+    ng-controller="PeopleController as vm"
+	data-dojo-props="data: vm.peopleData, onClick: this.onClick, $modules: 'people'"
+	onclick="this.onClick(event)">
+</people-directive>
+```
+##### Use Angular 'ng-repeat' together with an Angular controller
+What this example shows is that it is perfectly fine to use the adapter without a Angular directive component. Here we use a controller which provides data for the Angular repeater to make a list of people.
+```html
+<div data-dojo-type="AngularToDojoAdapter"
+    data-dojo-props="$modules: 'people'"
+    ng-controller="PeopleController as vm">
+
+    <div ng-repeat="model in vm.peopleData">
+        {{model.name}}
+    </div>
+</div>
+```
+## FAQ
+### Can I use two-way binding with this adapter?
+Two-way binding can be achieved by making use of ```dojox.mvc.at```. In 'data-dojo-props' you can add as value ```at(this, myProperty)``` which makes it bind both ways.
+### Can I also use Angular declarative syntax in HTML?
+Yes, you can use this adapter as wrapper around Angular templating like this:
+```html
+<div data-dojo-type="AngularToDojoAdapter"
+    data-dojo-props="$modules: [yourModuleDependenciesHere]">
+    <!-- Your Angular templating here. This will be parsed by Angular together with the adapter node -->
+</div>
+```
---
+++ src/AngularToDojoAdapter.js
@@ -0,0 +1,113 @@
+define(["dojo/_base/declare", "dijit/_WidgetBase"],
+	function(declare, _WidgetBase){
+		return declare([_WidgetBase], {
+			$modules: null,
+			$domNodeObserver: null,
+			scope: null,
+
+			constructor: function(attrs, domNode){
+				var i;
+				var attribute;
+				var attributeValueAsString;
+				var attributeNewName;
+				var eventPrefixRegExp = /^on(?=.+?)/;
+
+				// Loop over attributes and convert native event attributes to Angular variant
+				for(i=0;i<domNode.attributes.length;i++){
+					attribute = domNode.attributes[i];
+					attributeValueAsString = attribute.value.toString();
+
+					// We only need to handler event attributes.
+					if(eventPrefixRegExp.test(attribute.name)){
+						attributeNewName = attribute.name.replace(eventPrefixRegExp, "ng-");
+
+						// Add 'ng-' event attribute
+						attrs[attributeNewName] = attribute.value;
+						domNode.setAttribute(attributeNewName, attribute.value);
+
+						// Remove 'on' event attribute
+						delete attrs[attribute.name];
+						domNode.removeAttribute(attribute.name);
+					}
+				}
+			},
+
+			postCreate: function(){
+				this.inherited(arguments);
+
+				// Retrieve injector to bootstrap our adapter
+				var injector = angular.injector(["ng"].concat(this.$modules));
+
+				// Get our Angular dependencies to bootstrap the adapter
+				injector.invoke(["$compile", "$rootScope", function($compile, $rootScope){
+					var scopeKey;
+					var paramsKey;
+
+					// Delete Dojo or Angular 1.x Adapter specific data from the params.
+					delete this.params.$modules;
+					delete this.params.template;
+
+					// Create Adapter Scope to use for compiling the DOM element.
+					this.scope = $rootScope.$new();
+					angular.extend(this.scope, this.params);
+
+					// Add all Angular related data in the Adapter Scope as attribute on the DOM elment for compilation.
+					for(scopeKey in this.scope){
+						// Delete Dojo or Angular 1.x Adapter specific attributes from the Adapter Scope.
+						if(scopeKey !== "data-dojo-props" && this.domNode.attributes[scopeKey]){
+							delete this.scope[scopeKey];
+							continue;
+						}
+
+						if(this.scope.hasOwnProperty(scopeKey) && /^[a-z]/i.test(scopeKey)){
+							// Normalize property to attribute scopeKey, and set value.
+							this.domNode.setAttribute(scopeKey.split(/(?=[A-Z])/).join("-").toLowerCase(), scopeKey);
+
+							// Listen for changes to params and sync to adapter scope.
+							for(paramsKey in this.params){
+								this.watch(paramsKey, this._onParamsChange);
+							}
+
+							// Listen for changes on the Adapter Scope to bind back to the Dojo parent.
+							this.scope.$watch(scopeKey, this._onScopeChange.bind(this, scopeKey));
+						}
+					}
+
+					// Compile the DOM element
+					$compile(this.domNode)(this.scope, function(element, scope){
+						// Replace Dojo parsed node with Angular 1.x parsed node
+						this.domNode.parentNode.replaceChild(element[0], this.domNode);
+						this.domNode = element[0];
+					}.bind(this));
+				}.bind(this)]);
+			},
+
+			destroy: function(){
+				this.inherited(arguments);
+				this.domNodeObserver.disconnect();
+			},
+
+			// Changes from Angular 1.x
+			// ** Tech Note **: This does not work for mvc.at.to (single backward binding). However
+			// mvc.at.from (single forward binding) and mvc.at.both (two-way binding) are working.
+			_onScopeChange: function(prop, newValue, oldValue){
+				if(newValue === oldValue) { return; }
+
+				this.set(prop, newValue);
+			},
+
+			// Changes from Dojo
+			_onParamsChange: function(prop, oldValue, newValue){
+				if(newValue === oldValue) { return; }
+
+				this.scope[prop] = newValue;
+
+				try{
+					this.scope.$digest();
+				} catch(e){
+					console.info(e);
+				}
+			}
+		});
+	}
+);
